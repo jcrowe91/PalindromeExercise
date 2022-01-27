838aad03f0c954d18c9eea4803983ed553273994
jcrowe91 <jcrowe9119@gmail.com>
2022-01-27 13:37:16 -0600

    Finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        //In your WordSmith class, you will create a method(IsAPalindrome) 
+        //that will take a parameter of type string and return a value of type bool.  
+        //If the given input is a palindrome – return true, else return false. 
+        public bool IsAPalindrome(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            var reversed = new string(charArray);
+
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("racetruck", false)]
+        [InlineData("kayak", true)]
+        [InlineData("steamboat", false)]
+        public void Test1(string word, bool expected)
         {
-
+            var WordSmith = new WordSmith();
+            var actual = WordSmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
